24ec7d2b8baa8435f7c738eb36fcc12070b5fa1b
John Micheal Acera <john.acera@sportality.se>
2026-03-20 10:37:34 +0800

    refactor: simplify Prisma client creation by removing unnecessary Pool instantiation

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,4 @@
 import { PrismaPg } from "@prisma/adapter-pg";
-import { Pool } from "pg";
 import { PrismaClient } from "@/generated/prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
@@ -11,8 +10,7 @@ function createPrismaClient(): PrismaClient {
   if (!connectionString) {
     throw new Error("DATABASE_URL is not set");
   }
-  const pool = new Pool({ connectionString });
-  const adapter = new PrismaPg(pool);
+  const adapter = new PrismaPg({ connectionString });
   return new PrismaClient({ adapter });
 }
 
